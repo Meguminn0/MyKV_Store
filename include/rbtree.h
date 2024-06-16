94218b231db2cf10640c19b01a979d2153960b41
zding404 <3036537400@qq.com>
2024-06-16 21:36:32 +0800

    维护红黑树

--- include/rbtree.h
+++ include/rbtree.h
@@ -2,8 +2,8 @@
 #define RBTREE_H_
 
 typedef bool rbtree_node_color_type;        // 节点颜色
-typedef rbtree_node_color_type rbtree_node_red = true;      // 红色节点 
-typedef rbtree_node_color_type rbtree_node_black = false;   // 黑色节点
+static constexpr rbtree_node_color_type rbtree_node_red = true;      // 红色节点 
+static constexpr rbtree_node_color_type rbtree_node_black = false;   // 黑色节点
 
 typedef char* rbTree_node_key_type;     // key类型
 typedef char* rbTree_node_value_type;   // value类型
@@ -23,15 +23,146 @@ struct RBTreeNode
     value_type  value;  // 节点value值
 };
 
+// tree algorithm
+RBTreeNode* CreateNode(RBTreeNode::key_type key, RBTreeNode::value_type value)
+{
+    std::allocator<RBTreeNode> alltor;
+    RBTreeNode* node = new RBTreeNode();
+    node->key = key
+    node->value = value;
+    node->parent = nullptr;
+    node->left = nullptr;
+    node->right = nullptr;
+    node->color = rbtree_node_red:
+    
+    return node;
+}
+
+void InvertColor(RBTreeNode* node)
+{
+    node->color = !node->color;
+}
+
+RBTreeNode* GetBrother(RBTreeNode* node)
+{
+    bool is_left_node = ((node == node->parent->left) ? true : false);
+    if(is_left_node)
+    {
+        return node->parent->right;
+    }
+    else
+    {
+        return node->parent->left;
+    }
+}
+
+RBTreeNode* GetLeftMostNode(RBTreeNode* node)
+{
+    while(node->left != nullptr)
+    {
+        node = node->left;
+    }
+
+    return node;
+}
+
+RBTreeNode* GetRightMostNode(RBTreeNode* node)
+{
+    while(node->right != nullptr)
+    {
+        node = node->right;
+    }
+
+    return node;
+}
+
+LL_InsertRotate(RBTreeNode* node)
+{
+
+}
+
+RR_InsertRotate(RBTreeNode* node)
+{
+    
+}
+
+LR_InsertRotate(RBTreeNode* node)
+{
+    
+}
+
+RL_InsertRotate(RBTreeNode* node)
+{
+    
+}
+
+void MaintainAfterInsert(RBTreeNode* node)
+{
+    if(node->parent == nullptr && node->color == rbtree_node_red)
+    {
+        InvertColor(node);
+        return;
+    }
+
+    RBTreeNode* parent = node->parent;
+    assert(parent != nullptr);
+
+    if(parent->color == rbtree_node_red)
+    {
+        RBTreeNode* grandparent = parent->parent;
+        assert(grandparent != nullptr);
+        RBTreeNode* uncle = GetBrother(parent);
+
+        if(uncle != nullptr && uncle->color == rbtree_node_red)
+        {
+            InvertColor(uncle);
+            InvertColor(parent);
+            InvertColor(grandparent);
+
+            MaintainAfterInsert(grandparent);
+        }
+        else
+        {
+            bool parent_is_left = ((parent == grandparent->left) ? true : false);
+            bool node_is_left = ((node == parent->left) ? true : false);
+            if(parent_is_left)
+            {
+                if(node_is_left)
+                {
+                    LL_InsertRotate(node);
+                }
+                else
+                {
+                    LR_InsertRotate(node);
+                }
+            }
+            else
+            {
+                if(node_is_left)
+                {
+                    RL_InsertRotate(node);
+                }
+                else
+                {
+                    RR_InsertRotate(node);
+                }
+            }
+        }
+    }
+}
+
+void MaintainAfterRemove(RBTreeNode* node)
+{
+
+}
+
 class RBTree
 {
 public:
     typedef RBTreeNode::key_type    key_type;
     typedef RBTreeNode::value_type  value_type;
 
-    // 构造函数
     RBTree() : root_(nullptr), count_(0) { }
-    // 析构函数
     ~RBTree();
 
     int Insert(key_type key, value_type value);
@@ -39,8 +170,8 @@ public:
     RBTreeNode* search(key_type key);
 
 private:
-    RBTreeNode* root_;
-    size_t count_;
+    RBTreeNode* root_;  // 根节点
+    size_t count_;      // 所有节点个数
 };
 
 #endif
--- src/rbtree.cpp
+++ src/rbtree.cpp
@@ -1,25 +1,178 @@
+#include <stack>
 #include "rbtree.h"
 
 // 析构函数
 RBTree::~RBTree()
 {
-    
+    if(root_ != nullptr)
+    {
+        // 通过后序遍历delete所有节点
+        std::stack<RBTreeNode*> stk1;
+        std::stack<RBTreeNode*> stk2;
+        stk1.push(root_);
+        while(!stk1.empty())
+        {
+            RBTreeNode* node = stk1.top();
+            stk1.pop();
+            stk2.push(node);
+            if(node->left != nullptr)
+            {
+                stk1.push(node->left);
+            }
+            if(node->right != nullptr)
+            {
+                stk1.push(node->right);
+            }
+        }
+
+        while(!stk2.empty())
+        {
+            RBTreeNode* node = stk2.top();
+            stk2.pop();
+            delete node;
+        }
+    }
 }
 
-// 返回1成功，返回0失败
+// 成功返回1，失败返回0, 已存在返回-2
 int RBTree::Insert(key_type key, value_type value)
 {
+    if(root_ == nullptr)
+    {
+        root_ = CreateNode(key, value);
+        InvertColor(root_);
+        ++count_;
+        return 1;
+    }
     
+    RBTreeNode* insert_node = root_;
+    while(insert_node != nullptr)
+    {
+        if(strcmp(insert_node->key, key) > 0)
+        {
+            if(insert_node->right == nullptr)
+            {
+                insert_node->right = CreateNode(key, value);
+                insert_node->right->parent = insert_node;
+                MaintainAfterInsert(insert_node->right);
+                ++count_;
+                return 1;
+            }
+            insert_node = insert_node->right;
+        }
+        else if(strcmp(insert_node.key, key) < 0)
+        {
+            if(insert_node->left == nullptr)
+            {
+                insert_node->left = CreateNode(key, value);
+                insert_node->left->parent = insert_node;
+                MaintainAfterInsert(insert_node->left);
+                ++count_;
+                return 1;
+            }
+            insert_node = insert_node->left;
+        }
+        else
+        {
+            return -2;
+        }
+    }
+
+    return 0;
 }
 
 // 返回1成功，返回0失败
 int RBTree::remove(key_type key)
 {
-    
+    RBTreeNode* remove_node = search(key);
+    if(remove_node != nullptr)
+    {
+        if(remove_node->left != nullptr && remove_node->right != nullptr)
+        {
+            RBTreeNode* replace_node = GetLeftMostNode(remove_node->left);
+            remove_node->key = replace_node->key;
+            remove_node->value = replace_node->value;
+            remove_node = replace_node;
+        }
+        
+        if(remove_node->left != nullptr && remove_node->right == nullptr ||
+                remove_node->left == nullptr && remove_node->right != nullptr)
+        {
+            RBTreeNode* parent = remove_node->parent;
+            RBTreeNode* child_node = nullptr;
+            bool is_left_node = ((remove_node == parent->left) ? true : false);
+            if(remove_node->left != nullptr)
+            {
+                child_node = remove_node->left;
+            }
+            else
+            {
+                child_node = remove_node->right;
+            }
+
+            child_node->parent = parent;
+            if(is_left_node)
+            {
+                parent->left = child_node;
+            }
+            else
+            {
+                parent->right = child_node;
+            }
+            InvertColor(rchild_node);
+        }
+        else
+        {
+            RBTreeNode* parent = remove_node->parent;
+            bool is_left_node = ((remove_node == parent->left) ? true : false);
+            if(remove_node == root_)
+            {
+                root_ = nullptr;
+            }
+            else
+            {
+                if(remove_node->color == rbtree_node_black)
+                {
+                    MaintainAfterRemove(remove_node);
+                }
+
+                if(is_left_node)
+                {
+                    parent->left = nullptr;
+                }
+                else
+                {
+                    parent->right = nullptr;
+                }
+            }
+        }
+        delete remove_node;
+        --count_;
+        return 1;
+    }
+
+    return 0;
 }
 
-// 返回1成功，返回0失败，返回-1已经存在
-RBTreeNode* RBTree::search(key_type key)
+// 成功返回指针，失败返回nullptr
+RBTreeNode* RBTree::Search(key_type key)
 {
-    
+    RBTreeNode* search_node = root_;
+    while(search_node != nullptr)
+    {
+        if(strcmp(search_node->key, key) < 0)
+        {
+            search_node = search_node->left;
+        }
+        else if(strcmp(search_node->key, key) > 0)
+        {
+            search_node = search_node->right;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return search_node;
 }
